5d878cdd5e27df498bc0cc7fb024baab7e8953ee
Brett Taylor <brett@webfroot.co.nz>
2014-04-08 13:59:24 +1200

    game functionally complete

--- memnon.js
+++ memnon.js
@@ -2,6 +2,9 @@
 
 	var symbol = "&#10050;"; // aztec sun ❂
 	var board = [];
+	var gameState = "gameover";
+	var foundCoins = 0;
+	var gameTimeout;
 
 	function initBoard() {
 		board = [];
@@ -14,13 +17,16 @@
 		for (var i = 0; i < 49; i += 1) {
 			var b = document.querySelector("#grid_" + i);
 			b.innerHTML = "";
+			b.disabled = false;
 		}
 	}
 
 	function showBoard () {
 		for (var i = 0; i < 49; i += 1) {
 			var b = document.querySelector("#grid_" + i);
-			b.innerHTML = board[i];
+			if (board[i]) {
+				b.innerHTML = board[i];
+			}
 		}
 	}
 
@@ -44,13 +50,54 @@
 
 	function container_click(evt) {
 		if (evt.target.tagName.toLowerCase() === "button") {
-			//console.log("clicked button #" + evt.target.id);
 			var index = evt.target.id.split("_");
 			index = (+index[1]);
-			//console.log(index);
-			if (hasCoin(index)) {
-				console.log(index + " has a coin!");
-			}
+
+			switch (gameState) {
+				case "show":
+					startGuess();
+					break;
+				case "guess":
+					enterGuess(index);
+					break;
+				case "gameover":
+					// no break
+				case "win":
+					startGame();
+					break;
+				default:
+			} // end switch
+		}
+	}
+	function enterGuess(index) {
+		//console.log(index);
+		if (hasCoin(index)) {
+			console.log(index + " has a coin!");
+			foundCoins += 1;
+			var b = document.querySelector("#grid_" + index);
+			b.innerHTML = symbol;
+			b.disabled = true;
+		} else {
+			showBoard();
+			var b = document.querySelector("#grid_" + index);
+			b.innerHTML = "&times;";
+			startGameOver();
+		}
+		checkForWin();
+	}
+
+	function startGameOver () {
+		clearTimeout(gameTimeout);
+		showBoard();
+		gameState = "gameover";
+		console.log("Game over.");
+
+	}
+
+	function checkForWin() {
+		if (foundCoins === 7) {
+			gameState = "win";
+			console.log("You win!");
 		}
 	}
 
@@ -58,9 +105,28 @@
 		return !!board[index];
 	}
 
+	function startGame() {
+		gameState = "show";
+		clearBoard();
+		shuffleBoard(7);
+		showBoard();
+		gameTimeout = setTimeout(startGuess, 7000); // ms
+	}
+
+	function startGuess() {
+		// prevent restarting guess mode
+		if (gameState === "guess") { return; } 
+
+		gameState = "guess";
+		foundCoins = 0;
+		clearBoard();
+		
+		clearTimeout(gameTimeout);
+		gameTimeout = setTimeout(startGameOver, 7000);
+	}
+
 	addButtonListeners();
 
-	shuffleBoard(7);
-	showBoard();
+	//startGame();
 
 })();
